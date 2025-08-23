3e6093b5013a33713803c8424ea205b70ddb5b05
YuriiHorbach <yuriihorbach.career@gmail.com>
2025-08-23 18:41:30 +0200

    Change devise views, specify devise routes path name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: "secure"
   # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
