ade95c177ec0051a5f7eafb91ae8f6d84b393a79
YuriiHorbach <yuriihorbach.career@gmail.com>
2025-11-27 22:45:28 +0100

    Add entry model validations
    Show errors on the view

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,14 @@
 class Entry < ApplicationRecord
   belongs_to :user
+
+  validates :name, :username, :password, presence: true
+  validate :validate_url
+
+  private
+
+  def validate_url
+    unless url.include? ("http" || "https")
+      errors.add(:url, "must be valid")
+    end
+  end
 end
